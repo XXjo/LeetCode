2ed0d947df9a29f5bb2fa389e8559d4a2e49e044
jojo <275959399@qq.com>
2021-08-09 00:04:49 +0800

    144 145

--- js/144. 二叉树的前序遍历.js
+++ js/144. 二叉树的前序遍历.js
@@ -4,7 +4,7 @@
  * @Autor: XuXiaoling
  * @Date: 2021-08-03 00:00:33
  * @LastEditors: XuXiaoling
- * @LastEditTime: 2021-08-03 00:13:52
+ * @LastEditTime: 2021-08-08 16:53:45
  */
 function TreeNode(val, left, right) {
     this.val = (val===undefined ? 0 : val)
@@ -12,14 +12,63 @@ function TreeNode(val, left, right) {
     this.right = (right===undefined ? null : right)
 }
 
+
 /**
+ * @description:    使用迭代，先将根节点入栈，出栈一个元素，然后将出栈元素的右节点和左节点一次入栈，
+ *                  重复上述操作，直至栈为空
+ *                  时间复杂度:O(n),空间复杂度:O(n)
  * @param {TreeNode} root
  * @return {number[]}
+ * @author: XuXiaoling
  */
 var preorderTraversal = function(root) {
-    var stack = [];
-    var res = [];
-    let cur = root;
-
-
+    let res = [];
+    let stack = [];
+    let cur = null;
+    if(root !== null) {
+        stack.push(root);
+        while(stack.length > 0) {
+            cur = stack.pop();
+            res.push(cur.val);
+            if(cur.right !== null) {
+                stack.push(cur.right);
+            }
+            if(cur.left !== null) {
+                stack.push(cur.left);
+            }
+        }
+    }
+    return res;
 };
+
+
+var recursion = function(root, res) {
+    if(root === null) {
+        return res;
+    }
+    else {
+        res.push(root.val);
+        recursion(root.left, res);
+        recursion(root.right, res);
+        return res;
+    }
+}
+
+var preorderTraversal2 = function(root) {
+    let res = [];
+    res = recursion(root, res);
+    return res;
+};
+
+let node5 = new TreeNode(5);
+let node4 = new TreeNode(4);
+let node3 = new TreeNode(3);
+let node2 = new TreeNode(2);
+let node1 = new TreeNode(1);
+let node0 = new TreeNode(0);
+node0.left = node1;
+node0.right = node2;
+node1.left = node3;
+node1.right = node4;
+node2.right = node5;
+console.log(preorderTraversal2(node0));
--- js/145. 二叉树的后序遍历.js
+++ js/145. 二叉树的后序遍历.js
@@ -4,5 +4,75 @@
  * @Autor: XuXiaoling
  * @Date: 2021-08-03 00:00:49
  * @LastEditors: XuXiaoling
- * @LastEditTime: 2021-08-03 00:00:49
+ * @LastEditTime: 2021-08-08 18:20:43
  */
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
+var recursion = function(root, res) {
+    if(root === null) {
+        return res;
+    }
+    else {
+        recursion(root.left, res);
+        recursion(root.right, res);
+        res.push(root.val);
+    }
+}
+/**
+ * @description: 使用递归
+ * @param {TreeNode} root
+ * @return {number[]} res
+ * @author: XuXiaoling
+ */
+var postorderTraversal = function(root) {
+    let res = [];
+    recursion(root, res);
+    return res
+};
+
+
+/**
+ * @description:    使树的节点入栈，当该节点没有左孩子且没有有孩子|| 该节点左孩子或右孩子已经被输出时，
+ *                  出栈，并且将出栈的节点值放入res中
+ *                  时间复杂度O(n),空间复杂度O(n)
+ * @param {*} root
+ * @return {number[]}
+ * @author: XuXiaoling
+ */
+var postorderTraversal2 = function(root) {
+    let res = [];
+    let stack = [];
+    if(root !== null) {
+        let cur = root;
+        stack.push(root);
+        while(stack.length > 0) {
+            let top = stack[stack.length - 1];
+            if((top.left === null && top.right === null) || top.left === cur || top.right === cur) {
+                cur = stack.pop();
+                res.push(cur.val);
+            }
+            else {
+                if(top.right) {
+                    stack.push(top.right);
+                }
+                if(top.left) {
+                    stack.push(top.left);
+                }
+            }
+        }
+    }
+    return res;
+}
+
+
+
+let node3 = new TreeNode(3);
+let node2 = new TreeNode(2);
+let node1 = new TreeNode(1);
+node1.right = node2;
+node2.left = node3;
+console.log(postorderTraversal2(node1));
